020aa7e210a1c87c78e318249f441ba62f74a80f
tcy8021 <tsuchiya1005a@gmail.com>
2021-11-16 03:12:00 +0900

    chore: コメント修正

--- src/atcoder/problems/abc045c.py
+++ src/atcoder/problems/abc045c.py
@@ -5,7 +5,7 @@ max_bit = 2 ** (len(s) - 1)  # +が入るパターンの総数
 for i in range(max_bit):
     left = 0  # 足し始めのインデックス
 
-    # +が入り得る場所に2進数を割り当て、値が1のときは+が入るものとみなして足していく
+    # +が入り得る場所に2進数を割り当て、値が1のときは+が入るとみなして足していく
     # +が入り得る場所の数に合わせて0埋めする
     for j, val in enumerate(format(i, "b").zfill(len(s) - 1)):
         if val == "1":
@@ -15,7 +15,7 @@ for i in range(max_bit):
             # 足し始めを次のインデックスに設定
             left = j + 1
 
-    # left が途中なら残りを足す
+    # 最後の+から残りの部分を答えに加算
     if left < len(s):
         ans += int(s[left:])
 
